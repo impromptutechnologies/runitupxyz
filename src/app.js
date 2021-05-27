e3654bbe59d22ab7fc6bafcd6d36b54bfe7998b1
Jongbin Won <jongbinwon@192.168.2.4>
2021-05-27 17:17:43 +0700

    really final

--- src/app.js
+++ src/app.js
@@ -312,8 +312,8 @@ app.post('/pay', (req, res) => {
           "payment_method": "paypal"
       },
       "redirect_urls": {
-          "return_url": "http://localhost:3000/success",
-          "cancel_url": "http://localhost:3000/cancel"
+          "return_url": "https://meow-web.herokuapp.com/success",
+          "cancel_url": "https://meow-web.herokuapp.com/cancel"
       },
       "transactions": [{
           "item_list": {
