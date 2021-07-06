a6fb3ce57aea3bed47407b834fed69c2331759f8
Jongbin Won <jongbinwon@192.168.2.4>
2021-07-06 21:52:26 +0700

    d

--- src/utils/cryptopriceopen.js
+++ src/utils/cryptopriceopen.js
@@ -1,11 +1,11 @@
 const request = require("request");
 const Crypto = require("../models/cryptoSchema");
 const rp = require('request-promise');
+const CoinMarketCap = require('coinmarketcap-api')
 
 const cryptoPriceOpen = () => {
     Crypto.find({}, (error, cryptos) => {
       cryptos.forEach((crypto) => {
-          const CoinMarketCap = require('coinmarketcap-api')
           const apiKey = process.env.COIN_API
           const client = new CoinMarketCap(apiKey)
           client.getQuotes({symbol: crypto.symbol, convert: 'USD'}).then(response => {
